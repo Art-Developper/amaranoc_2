a1ca7cb15fd9392efa6042641fdf3e3cb19a1fb0
Artyom Santoyan <artyomsantoyan.ait@gmail.com>
2026-01-24 14:17:25 +0400

    start

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
-
+import Image from "next/image"
 
 export default function Home() {
   return (
     <>
-      <div>
-        <div>
-          
+      <div className="flex gap-96 my-5">
+        <div className="flex items-start justify-start">
+          <img src="/logo.svg" alt="Logo" width="150"/>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex items-center justify-center">
+          <a className="">Գլխավոր</a>
+          <a>Զեղչեր</a>
+          <a>Ծառայություններ</a>
+          <a>Մեր մասին</a>
+        </div>
+        <div className="flex justify-end items-end"></div>
       </div>
     </>
   );
