4cbb4894680327e24ceb171e015ae6c1e5cdf842
Artyom Santoyan <artyomsantoyan.ait@gmail.com>
2026-01-29 17:19:09 +0400

    added database for sales and services

--- src/app/sales/page.js
+++ src/app/sales/page.js
@@ -5,6 +5,7 @@ import { User, Globe, Search, Facebook, Instagram, Phone, Mail, MapPin, Heart, C
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
+import { discounts, prices, houses } from './salesData';
 
 export default function Sales() {
   const pathName = usePathname();
@@ -13,35 +14,6 @@ export default function Sales() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [bookingStep, setBookingStep] = useState(1);
   const [formData, setFormData] = useState({ recipientName: '', phoneNumber: '' });
-
-  const discounts = [
-    {
-      percent: "-15%",
-      title: "2 կամ ավել ամրագրումի օրերի դեպքում",
-      desc: "Ստացեք 5-15% զեղչ կախված ամրագրումի 3-ից մինիմում 20 օր:",
-      img: "https://amaranoc.am/images/raffle/special-discounts-image.jpg",
-    },
-    {
-      percent: "-10%",
-      title: "Անձնական Reel-ի հրապարակման դեպքում",
-      desc: "Կիսվել մեր հյուրանոցում ձեր արձակուրդի օրերից՝ նշելով amaranoc.am և ստացեք 10% զեղչ:",
-      img: "https://amaranoc.am/images/raffle/special-discounts-image.jpg",
-    },
-    {
-      percent: "-5%",
-      title: "2-րդ այցելության դեպքում",
-      desc: "Վերադարձի դեպքում ստացեք 5% զեղչ 3-րդ ամրագրումի դեպքում:",
-      img: "https://amaranoc.am/images/raffle/special-discounts-image.jpg",
-    },
-  ];
-
-  const prices = ["50,000 ֏", "60,000 ֏", "70,000 ֏", "80,000 ֏", "90,000 ֏", "100,000 ֏"];
-
-  const houses = [
-    { id: 1, image: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1759149473223--0.33907271602966693image.webp&w=1920&q=75", location: "Բջնի", price: "40,000֏", people: "4-6", rating: "4.8" },
-    { id: 2, image: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1753697519352--0.8706588573375771image.webp&w=1920&q=75", location: "Օհանավան", price: "75,000֏", people: "2-4", rating: "5.0" },
-    { id: 3, image: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1758095203425--0.034694092059661896image.webp&w=1920&q=75", location: "Ծաղկաձոր", price: "75,000֏", people: "6-10", rating: "4.9" },
-  ];
 
   const linkClass = (path) =>
     `relative pb-1 transition-all
